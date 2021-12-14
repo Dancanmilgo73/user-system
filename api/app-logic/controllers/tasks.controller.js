4899222f184c857fc20c49da2834d6ac4bd4c43a
Dancanmilgo73 <dancanmilgo73@gmail.com>
2021-12-14 11:45:57 +0300

    worked on the tasks controller

--- api/app-logic/controllers/tasks.controller.js
+++ api/app-logic/controllers/tasks.controller.js
@@ -36,10 +36,94 @@ const getTaskById = async (req, res) => {
 			"project_Id",
 			"userId",
 		]);
-	
+
 		res.status(200).json(task);
 	} catch (error) {
 		res.status(500).send({ message: error.message });
 	}
 };
-module.exports = { getAllTasks, getTaskById };
+const assignTask = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { userId } = req.body;
+		const pool = await mssql.connect(sqlConfig);
+		const data = await pool
+			.request()
+			.input("taskId", mssql.Int, id)
+			.input("userId", mssql.Int, userId)
+			.execute("dbo.spTasks_AssignTask");
+		res
+			.status(200)
+			.send({ message: `Task asingned to the user with the id of ${userId}` });
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
+};
+const addTask = async (req, res) => {
+	try {
+		const { name, description, projectId } = req.body;
+		const pool = await mssql.connect(sqlConfig);
+		const data = await pool
+			.request()
+			.input("taskname", mssql.Int, name)
+			.input("projectId", mssql.Int, projectId)
+			.input("taskDescription", mssql.VarChar, description)
+			.execute("dbo.spTasks_AddTask");
+		res.status(200).send({ message: `Task created successfully` });
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
+};
+const deleteTask = async (req, res) => {
+	try {
+		const { id } = req.param;
+		const pool = await mssql.connect(sqlConfig);
+		const data = await pool
+			.request()
+			.input("id", mssql.Int, id)
+			.execute("dbo.spTasks_deleteTask");
+		res.status(200).send({ message: `Task with id ${id} was deleted` });
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
+};
+const updateTask = async (req, res) => {
+	const { newName, newDescription } = req.body;
+	if (!newName && !newDescription)
+		return res
+			.status(401)
+			.send({ message: "You have to provide the new details to be update" });
+	try {
+		const { id } = req.param;
+		const pool = await mssql.connect(sqlConfig);
+		const data = await pool
+			.request()
+			.input("id", mssql.Int, id)
+			.execute("dbo.spTasks_GetTask");
+		const task = _.pick(data.recordset[0], [
+			"taskName",
+			"taskDescription",
+			"project_Id",
+			"userId",
+		]);
+		await pool
+			.request()
+			.input("id", mssql.Int, id)
+			.input('name', mssql.VarChar, newName ? newName : task.taskName)
+			.input('description', mssql.VarChar,newDescription ? newDescription : task.description)
+			.execute("dbo.spTasks_updateTask");
+		res.status(200).send({ message: `Task with id ${id} was updated` });
+	} catch (error) {
+		res.status(500).send({ message: error.message });
+	}
+};
+const submitComplete = async (req, res) =>
+{
+	try {
+		
+	} catch (error) {
+		
+	}
+}
+
+module.exports = { getAllTasks, getTaskById, assignTask, deleteTask, updateTask, submitComplete, addTask };
