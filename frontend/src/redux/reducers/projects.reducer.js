c65c9e23292647ed572692fe3dbf3af8e1980b6f
Dancanmilgo73 <dancanmilgo73@gmail.com>
2021-12-15 12:07:20 +0300

    fixing a typo

--- frontend/src/redux/reducers/projects.reducer.js
+++ frontend/src/redux/reducers/projects.reducer.js
@@ -1,28 +1,35 @@
-import { GET_PROJECTS_FAIL, GET_PROJECTS_REQUEST, GET_PROJECTS_SUCCESS } from "../actionTypes";
+import {
+	GET_PROJECTS_FAIL,
+	GET_PROJECTS_REQUEST,
+	GET_PROJECTS_SUCCESS,
+} from "../actionTypes";
 
 const initialState = {
-    loasing: false,
-    projects: [],
-    error: null
-}
+	loading: false,
+	projects: [],
+	error: null,
+};
 
-export const projectsReducer = (state = initialState, { type, payload }) =>
-{
-    switch (type)
-    {
-        case GET_PROJECTS_REQUEST:
-            return {
-                ...state, loading: true
-            };
-        case GET_PROJECTS_SUCCESS:
-            return {
-                ...state, loading: false, projects: [...payload]
-            };
-        case GET_PROJECTS_FAIL:
-            return {
-                ...state, loading: false, error: payload
-            };
-        default:
-            return state;
-    }
-}
+export const projectsReducer = (state = initialState, { type, payload }) => {
+	switch (type) {
+		case GET_PROJECTS_REQUEST:
+			return {
+				...state,
+				loading: true,
+			};
+		case GET_PROJECTS_SUCCESS:
+			return {
+				...state,
+				loading: false,
+				projects: [...payload],
+			};
+		case GET_PROJECTS_FAIL:
+			return {
+				...state,
+				loading: false,
+				error: payload,
+			};
+		default:
+			return state;
+	}
+};
